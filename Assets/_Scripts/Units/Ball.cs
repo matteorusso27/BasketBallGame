fc45d86b1718dc4311a567af5a8b2dc598b9687a
Matteo <matteo2795@gmail.com>
2024-04-16 14:05:28 +0200

    implement IMoveable interface

--- Assets/_Scripts/Units/Ball.cs
+++ Assets/_Scripts/Units/Ball.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviour, IMoveable
 {
-    // Start is called before the first frame update
+    public void Move(Vector3 toPosition)
+    {
+        
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+}
+
+public interface IMoveable
+{
+    void Move(Vector3 toPosition);
 }
