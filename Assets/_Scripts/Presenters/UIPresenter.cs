c7db6f438f0300d8dad4357cb776e750c7c0e238
matteorusso27 <matteo2795@gmail.com>
2024-04-29 22:57:04 +0200

    added ui presenter

--- Assets/_Scripts/Presenters/UIPresenter.cs
+++ Assets/_Scripts/Presenters/UIPresenter.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using static Selectors;
-public class ScorePresenter : MonoBehaviour
+public class UIPresenter : MonoBehaviour
 {
+    public TMP_Text Time;
     public TMP_Text PlayerScore;
     public TMP_Text EnemyScore;
     private void Start()
     {
         ScoreM.PlayerScoreUpdate += (int a) => PlayerScore.text = "Player: "+a.ToString(); 
         ScoreM.EnemyScoreUpdate += (int a) => EnemyScore.text = "Enemy: "+a.ToString();
+        GameM.OnTimerChange += (int a) => Time.text = "Time: "+a.ToString();
     }
 }
